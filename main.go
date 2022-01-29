6a4ea7f39b36988cd0cdc3310c615be336fb3523
Andrew Klotz <agc.klotz@gmail.com>
2022-01-29 01:28:40 +0000

    adding empty display window

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"syscall"
+	"time"
 
 	"actions-per-minute-tracker/win32"
 )
@@ -33,8 +36,13 @@ func (c *callback) mouseCallback(code int, wparam win32.WPARAM, lparam win32.LPA
 	return win32.CallNextHookEx(c.hookMouse, code, wparam, lparam)
 }
 
+func windowProc(hwnd syscall.Handle, msg uint32, wparam win32.WPARAM, lparam win32.LPARAM) win32.LRESULT {
+	return win32.DefWindowProc(hwnd, msg, wparam, lparam)
+}
+
 func main() {
 	fmt.Println("starting main...")
+	// setup apm tracker
 	cb := callback{}
 	hookKeyboard := win32.SetWindowsHookEx(win32.WH_KEYBOARD_LL, cb.keyboardCallback, 0, 0)
 	hookMouse := win32.SetWindowsHookEx(win32.WH_MOUSE_LL, cb.mouseCallback, 0, 0)
@@ -47,6 +55,68 @@ func main() {
 	cb.hookKeyboard = hookKeyboard
 	cb.hookMouse = hookMouse
 
+	// setup window
+	className := "apm-window-object"
+
+	instance, err := win32.GetModuleHandle()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	cursor, err := win32.LoadCursorResource(win32.IDC_ARROW)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	wndClass := win32.NewWNDClasss(className, windowProc, instance, cursor)
+	if _, err = win32.RegisterClassEx(&wndClass); err != nil {
+		log.Println(err)
+		return
+	}
+
+	height := 35
+	width := 130
+	var extraStyles uint32 = win32.WS_EX_COMPOSITED | win32.WS_EX_LAYERED | win32.WS_EX_NOACTIVATE | win32.WS_EX_TOPMOST | win32.WS_EX_TRANSPARENT
+	var styles uint32 = win32.WS_VISIBLE | win32.WS_POPUP
+	hwnd := win32.CreateWindow(
+		extraStyles, // extra style
+		className,
+		"Test Pupper Window", // name
+		uint32(styles),       // style
+		int64(win32.GetSystemMetrics(win32.SM_CXSCREEN))-int64(width), // x
+		int64(height*2), // y
+		int64(width),    // width
+		int64(height),   // height
+		0,               // parent
+		0,               // menu
+		instance,
+	)
+
+	win32.SetWindowPos(
+		hwnd,
+		^win32.HWND(0),
+		0,
+		0,
+		0,
+		0,
+		win32.SWP_NOACTIVATE|win32.SWP_NOMOVE|win32.SWP_NOSIZE|win32.SWP_SHOWWINDOW,
+	)
+
+	counter := 1000
+	ticker := time.NewTicker(1000 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				win32.SendMessage(hwnd, 35001, 0, 0)
+				counter++
+			}
+		}
+	}()
+
+	// pull messages
 	var msg win32.MSG
 	for {
 		fmt.Println("looping...")
@@ -55,6 +125,8 @@ func main() {
 			fmt.Println("bad msg val", msgVal)
 			break
 		}
-		fmt.Println("got msg", msg.WParam)
+		fmt.Println("got msg", msg)
+		win32.TranslateMessage(&msg)
+		win32.DispatchMessage(&msg)
 	}
 }
--- win32/win32.go
+++ win32/win32.go
@@ -11,6 +11,25 @@ var (
 	procUnhookWindowsHookEx = user32.NewProc("UnhookWindowsHookEx")
 	procGetMessage          = user32.NewProc("GetMessageW")
 	procCallNextHookEx      = user32.NewProc("CallNextHookEx")
+	pLoadCursorW            = user32.NewProc("LoadCursorW")
+
+	kernel32             = syscall.NewLazyDLL("kernel32.dll")
+	procGetModuleHandleW = kernel32.NewProc("GetModuleHandleW")
+
+	pDefWindowProcW   = user32.NewProc("DefWindowProcW")
+	pRegisterClassExW = user32.NewProc("RegisterClassExW")
+	pCreateWindowExW  = user32.NewProc("CreateWindowExW")
+	pGetSystemMetrics = user32.NewProc("GetSystemMetrics")
+	pSetWindowPos     = user32.NewProc("SetWindowPos")
+	pSendMessage      = user32.NewProc("SendMessageW")
+
+	pTranslateMessage = user32.NewProc("TranslateMessage")
+	pDispatchMessageW = user32.NewProc("DispatchMessageW")
+)
+
+const (
+	IDC_ARROW    = 32512
+	COLOR_WINDOW = 5
 )
 
 // https://docs.microsoft.com/en-us/windows/win32/inputdev/keyboard-input-notifications
@@ -56,6 +75,165 @@ type MSG struct {
 // http://msdn.microsoft.com/en-us/library/windows/desktop/dd162805.aspx
 type POINT struct {
 	X, Y int32
+}
+
+// GetSystemMetrics constants
+const (
+	SM_CXSCREEN = 0
+	SM_CYSCREEN = 1
+)
+
+// WindowFlags
+const (
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
+	WS_EX_TOPMOST     = 8
+	WS_EX_TRANSPARENT = 32
+	WS_EX_LAYERED     = 0x00080000
+	WS_EX_COMPOSITED  = 0x02000000
+	WS_EX_NOACTIVATE  = 0x08000000
+
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms632600(v=vs.85).aspx
+	WS_DISABLED = 134217728
+	WS_VISIBLE  = 268435456
+	WS_POPUP    = 0x80000000
+)
+
+const (
+	SWP_NOSIZE       = 0x0001
+	SWP_NOMOVE       = 0x0002
+	SWP_NOZORDER     = 0x0004
+	SWP_NOREDRAW     = 0x0008
+	SWP_NOACTIVATE   = 0x0010
+	SWP_FRAMECHANGED = 0x0020
+	SWP_SHOWWINDOW   = 0x0040
+)
+
+// https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexw
+type WNDCLASSEXW struct {
+	size       uint32
+	style      uint32
+	wndProc    uintptr
+	clsExtra   int32
+	wndExtra   int32
+	instance   syscall.Handle
+	icon       syscall.Handle
+	cursor     syscall.Handle
+	background syscall.Handle
+	menuName   *uint16
+	className  *uint16
+	iconSm     syscall.Handle
+}
+
+type HWND uintptr
+
+// https://docs.microsoft.com/en-us/windows/win32/learnwin32/writing-the-window-procedure
+type WindowProcFn func(syscall.Handle, uint32, WPARAM, LPARAM) LRESULT
+
+func NewWNDClasss(className string, windowProc WindowProcFn, instance, cursor syscall.Handle) WNDCLASSEXW {
+	wnclass := WNDCLASSEXW{
+		wndProc:    syscall.NewCallback(windowProc),
+		instance:   instance,
+		cursor:     cursor,
+		background: COLOR_WINDOW + 1,
+		className:  syscall.StringToUTF16Ptr(className),
+	}
+	wnclass.size = uint32(unsafe.Sizeof(wnclass))
+	return wnclass
+}
+
+func DefWindowProc(hwnd syscall.Handle, msg uint32, wparam WPARAM, lparam LPARAM) LRESULT {
+	ret, _, _ := pDefWindowProcW.Call(
+		uintptr(hwnd),
+		uintptr(msg),
+		uintptr(wparam),
+		uintptr(lparam),
+	)
+	return LRESULT(ret)
+}
+
+func RegisterClassEx(wcx *WNDCLASSEXW) (uint16, error) {
+	ret, _, err := pRegisterClassExW.Call(
+		uintptr(unsafe.Pointer(wcx)),
+	)
+	if ret == 0 {
+		return 0, err
+	}
+	return uint16(ret), nil
+}
+
+func CreateWindow(exStyle uint32, className, windowName string, style uint32, x, y, width, height int64, parent, menu, instance syscall.Handle) HWND {
+	ret, _, _ := pCreateWindowExW.Call(
+		uintptr(0),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(className))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(windowName))),
+		uintptr(style),
+		uintptr(x),
+		uintptr(y),
+		uintptr(width),
+		uintptr(height),
+		uintptr(parent),
+		uintptr(menu),
+		uintptr(instance),
+		uintptr(0),
+	)
+	return HWND(ret)
+}
+
+func GetSystemMetrics(index int) int {
+	ret, _, _ := pGetSystemMetrics.Call(
+		uintptr(index))
+
+	return int(ret)
+}
+
+func SetWindowPos(hwnd, hWndInsertAfter HWND, x, y, cx, cy int, uFlags uint) bool {
+	ret, _, _ := pSetWindowPos.Call(
+		uintptr(hwnd),
+		uintptr(hWndInsertAfter),
+		uintptr(x),
+		uintptr(y),
+		uintptr(cx),
+		uintptr(cy),
+		uintptr(uFlags))
+
+	return ret != 0
+}
+
+func SendMessage(hwnd HWND, msg int, wparam WPARAM, lparam LPARAM) LRESULT {
+	ret, _, _ := pSendMessage.Call(
+		uintptr(hwnd),
+		uintptr(msg),
+		uintptr(wparam),
+		uintptr(lparam),
+	)
+	return LRESULT(ret)
+}
+
+func DispatchMessage(msg *MSG) {
+	pDispatchMessageW.Call(uintptr(unsafe.Pointer(msg)))
+}
+
+func TranslateMessage(msg *MSG) {
+	pTranslateMessage.Call(uintptr(unsafe.Pointer(msg)))
+}
+
+func GetModuleHandle() (syscall.Handle, error) {
+	ret, _, err := procGetModuleHandleW.Call(uintptr(0))
+	if ret == 0 {
+		return 0, err
+	}
+	return syscall.Handle(ret), nil
+}
+
+func LoadCursorResource(cursorName uint32) (syscall.Handle, error) {
+	ret, _, err := pLoadCursorW.Call(
+		uintptr(0),
+		uintptr(uint16(cursorName)),
+	)
+	if ret == 0 {
+		return 0, err
+	}
+	return syscall.Handle(ret), nil
 }
 
 // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowshookexw
